180696ad08a04b0d3ccfc645eb4b064c12f96069
Codehub169 <codehub@169pi.com>
2025-06-11 12:24:24 +0530

    Applied user request:
    This code was run inside of a docker container, the container stopped due to some issue or something else happenedThis was the server log: Updating package list and installing dependencies (ffmpeg,

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Update package list and install ffmpeg and pip
-echo "Updating package list and installing dependencies (ffmpeg, python3-pip)..."
+echo "Updating package list and installing dependencies (apt-utils, ffmpeg, python3-pip)..."
 apt-get update -y
-apt-get install -y ffmpeg python3-pip
+apt-get install -y apt-utils ffmpeg python3-pip
 
 # Install Python dependencies from requirements.txt
 if [ -f requirements.txt ]; then
