0aa56c314973c783568adbc875706a40c03ccea0
Codehub169 <codehub@169pi.com>
2025-06-11 12:41:57 +0530

    Applied user request:
    This code was run inside of a docker container, the container stopped due to some issue or something else happenedThis was the server log:
    
    ./startup.sh: line 1: syntax error near unexpected token

--- startup.sh
+++ startup.sh
@@ -1,1 +1,10 @@
-<content_string>
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Run the Streamlit application.
+# The 'exec' command replaces the shell process with the streamlit process,
+# which is good practice for container entrypoint scripts as it allows signals
+# to be passed correctly to the application.
+exec streamlit run app.py --server.address=0.0.0.0 --server.port=${PORT:-8501}
